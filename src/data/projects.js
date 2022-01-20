c502526ce7c0a59313d351adba8112e17657bf33
Junel <sacrojunel@gmail.com>
2022-01-20 12:00:35 +0800

    feat: project data

---
+++ src/data/projects.js
@@ -0,0 +1,18 @@
+export const project = [
+  {
+    id: 1,
+    title: "MensCorner",
+    desc: "An E-commerce Front End that uses json server for backend data",
+    image: "/images/home.jpg",
+    githubLink: "https://github.com/zacro14/ecommerce-frontend",
+    websiteLink: "https://menscorner.herokuapp.com/",
+  },
+  {
+    id: 2,
+    title: "Zhacks Foody",
+    desc: "An Food Delivery web app Front End that uses firestore for backend data",
+    image: "/images/zhacks foody.jpg",
+    githubLink: "https://github.com/zacro14/zhaks-foody",
+    websiteLink: "https://zhaks-foody.vercel.app/",
+  },
+];
