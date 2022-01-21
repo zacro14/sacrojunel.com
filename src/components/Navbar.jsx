a7dfce221751e10e2a457c176c12e4ac6d8807e0
Junel <sacrojunel@gmail.com>
2022-01-21 20:36:23 +0800

    chore: add logo and adjust font size

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,32 +1,62 @@
 import { DiCode, DiNetbeans } from "react-icons/di";
+import Logo from "../../public/logo/Logo-junelsacro.png";
 import { Box, Flex, Icon, Link, Stack, Text } from "@chakra-ui/react";
+import Image from "next/image";
 
 const Navbar = () => {
   const navItems = [
     {
       id: 1,
       title: "Projects",
+      icon: DiNetbeans,
+      size: "6",
     },
     {
       id: 2,
       title: "Tech Stacks",
+      icon: DiCode,
+      size: "8",
     },
   ];
   return (
     <>
-      <Box as={"header"} px={"10"} py={"5"}>
+      <Box as={"header"} px={"20"} py={"5"}>
         <Flex alignItems={"center"} justifyContent={"space-between"}>
-          <Link href="/">Logo</Link>
-          <Stack direction={"row"} as={"nav"} fontWeight={"semibold"}>
-            <Link px={"5"} display={"flex"} alignItems={"center"}>
-              <Icon as={DiNetbeans} w={4} h={4} />
-              <Text pl={"1"}>Projects</Text>
-            </Link>
+          <Link href="/">
+            <Box
+              pos={"relative"}
+              boxSize={"24"}
+              transition={" 0.3s ease-in-out"}
+              _hover={{
+                transform: "scale(1.2)",
+                transition: "0.3s ease-in 0s",
+              }}
+            >
+              <Image src={Logo} alt="logo" layout={"fill"} />
+            </Box>
+          </Link>
+          <Stack
+            direction={"row"}
+            as={"nav"}
+            fontWeight={"semibold"}
+            fontSize={"2xl"}
+          >
+            {navItems.map((item) => (
+              <Link
+                key={item.id}
+                px={"5"}
+                display={"flex"}
+                alignItems={"center"}
+              >
+                <Icon as={item.icon} boxSize={item.size} />
+                <Text pl={"1"}>{item.title}</Text>
+              </Link>
+            ))}
 
-            <Link px={"5"} display={"flex"} alignItems={"center"}>
-              <Icon as={DiCode} w={6} h={6} />
+            {/* <Link px={"5"} display={"flex"} alignItems={"center"}>
+              <Icon as={} w={6} h={6} />
               <Text pl={"1"}>Tech Stacks</Text>
-            </Link>
+            </Link> */}
           </Stack>
         </Flex>
       </Box>
