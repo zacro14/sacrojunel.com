13d4eaef0127ed229d0e88e30f07aba1bff7ff5c
Junel <sacrojunel@gmail.com>
2022-01-22 17:41:33 +0800

    feat: add animation

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,64 +1,68 @@
 import PageContainer from "../components/PageContainer";
 import Project from "../components/Project";
 import { project } from "../data/projects";
-import {
-  Box,
-  Divider,
-  Grid,
-  GridItem,
-  Heading,
-  Stack,
-  Text,
-} from "@chakra-ui/react";
+import Seo from "../components/Seo";
+import { motion } from "framer-motion";
+import { Box, Divider, Heading, Stack, Text } from "@chakra-ui/react";
+
+const MotionText = motion(Text);
 
 export default function Home() {
   return (
     <>
+      <Seo title={"Junel Sacro | - Porfolio"} description={"This is a"} />
       <PageContainer>
-        <Box mx={{ base: "10", md: "20", lg: "20" }} zIndex={1}>
-          <Heading
-            display={"flex"}
-            flexDir={"column"}
-            justifyContent={"center"}
-            as={"h1"}
-            textTransform={"lowercase"}
-            fontSize={{ base: "4xl", md: "6xl", lg: "6xl" }}
-            letterSpacing={"tight"}
-            fontWeight={"bold"}
-            textAlign={"center"}
-            minH={{
-              base: "container.sm",
-              md: "container.sm",
-              lg: "container.sm",
-            }}
-          >
-            <Text
-              as={"span"}
-              textTransform={"capitalize"}
-              fontSize={{ base: "6xl", md: "9xl", lg: "9xl" }}
+        <Box mx={{ base: "5", md: "20", lg: "20" }} zIndex={1}>
+          <Box minH={"100vh"}>
+            <Heading
+              display={"flex"}
+              flexDir={"column"}
+              justifyContent={"center"}
+              as={"h1"}
+              fontSize={{ base: "4xl", md: "6xl", lg: "6xl" }}
+              letterSpacing={"tight"}
+              fontWeight={"bold"}
+              textAlign={"center"}
+              minH={{
+                base: "container.sm",
+                md: "container.sm",
+                lg: "container.sm",
+              }}
             >
-              Junel Sacro
-            </Text>
-            <Divider border={"1px"} borderColor={"beige"} />
-            <Text
-              as={"span"}
-              bgGradient="linear(to-l, cyan.100, cyan.800)"
-              bgClip="text"
-              pt={"5"}
-            >
-              a Front-End
-            </Text>
-            <Text
-              as={"span"}
-              bgGradient="linear(to-l, cyan.100, cyan.800)"
-              bgClip={"text"}
-            >
-              Web developer
-            </Text>
-          </Heading>
+              <Text
+                as={"span"}
+                textTransform={"capitalize"}
+                fontSize={{ base: "6xl", md: "9xl", lg: "9xl" }}
+              >
+                Junel Sacro
+              </Text>
+              <Divider border={"1px"} borderColor={"beige"} />
+              <MotionText
+                as={"span"}
+                bgGradient="linear(to-l, cyan.100, cyan.800)"
+                _hover={{
+                  bgGradient: "linear(to-b, orange.100, purple.300)",
+                }}
+                bgClip="text"
+                pt={"5"}
+                whileHover={{ scale: 1.1 }}
+              >
+                a Front-End
+              </MotionText>
+              <Text
+                as={"span"}
+                bgGradient="linear(to-l, cyan.100, cyan.800)"
+                bgClip={"text"}
+              >
+                Web developer
+              </Text>
+            </Heading>
+          </Box>
           <Box as={"section"} id={"projects"} height={"auto"}>
-            <Heading fontSize={"5xl"}>Featured Projects</Heading>
-            <Box mt={"10"}>
+            <Heading fontSize={{ base: "4xl", md: "5xl" }}>
+              Featured Projects
+            </Heading>
+            <Box mt={"20"}>
               <Stack>
                 {project.map((project) => (
                   <Project key={project.id} data={project} />
