e364a1322c49e6765231e0c33ceb5ad3b67aa4af
Junel <sacrojunel@gmail.com>
2022-01-22 22:47:47 +0800

    feat: add herosection

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,64 @@
+import { GiSharpCrown } from "react-icons/gi";
+import { Box, Divider, Heading, Icon, Text } from "@chakra-ui/react";
+import { motion } from "framer-motion";
+
+const MotionText = motion(Text);
+
+const Hero = () => {
+  return (
+    <>
+      <Box minH={"100vh"}>
+        <Heading
+          textAlign={"center"}
+          fontWeight={{ base: "normal", md: "medium" }}
+          letterSpacing={"widest"}
+          fontSize={{ base: "lg" }}
+        >
+          <Icon color={"cyan.100"} as={GiSharpCrown}></Icon> Welcome to my
+          Domain <Icon color={"cyan.100"} as={GiSharpCrown}></Icon>
+        </Heading>
+
+        <Heading
+          display={"flex"}
+          flexDir={"column"}
+          justifyContent={"center"}
+          as={"h1"}
+          fontSize={{ base: "4xl", md: "6xl", lg: "6xl" }}
+          letterSpacing={"tight"}
+          fontWeight={"bold"}
+          textAlign={"center"}
+        >
+          <Text
+            as={"span"}
+            textTransform={"capitalize"}
+            fontSize={{ base: "6xl", md: "9xl", lg: "9xl" }}
+          >
+            Junel Sacro
+          </Text>
+          <Divider border={"1px"} borderColor={"beige"} />
+          <MotionText
+            as={"span"}
+            bgGradient="linear(to-l, cyan.100, cyan.800)"
+            _hover={{
+              bgGradient: "linear(to-b, orange.100, purple.300)",
+            }}
+            bgClip="text"
+            pt={"5"}
+            whileHover={{ scale: 1.1 }}
+          >
+            A Front-End
+          </MotionText>
+          <Text
+            as={"span"}
+            bgGradient="linear(to-l, cyan.100, cyan.800)"
+            bgClip={"text"}
+          >
+            Web developer
+          </Text>
+        </Heading>
+      </Box>
+    </>
+  );
+};
+
+export default Hero;
