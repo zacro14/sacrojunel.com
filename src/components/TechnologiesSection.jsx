82c914be7e5718669f592b4dd62727273ce4ea0a
Junel <sacrojunel@gmail.com>
2022-01-23 20:10:36 +0800

    chore: fixed padding and add description

--- src/components/TechnologiesSection.jsx
+++ src/components/TechnologiesSection.jsx
@@ -1,5 +1,14 @@
 import { items } from "../data/tecnologies";
-import { Box, Container, Flex, Heading, Icon, Stack } from "@chakra-ui/react";
+import {
+  Box,
+  Container,
+  Flex,
+  Heading,
+  Icon,
+  Link,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
 
 const ItemCard = ({ item }) => {
   return (
@@ -10,19 +19,29 @@ const ItemCard = ({ item }) => {
       p={"5"}
       rounded={"2xl"}
     >
-      <Heading textTransform={"capitalize"} fontSize={"large"} pb={"5"}>
+      <Heading
+        textTransform={"capitalize"}
+        fontSize={"large"}
+        pb={"5"}
+        textAlign={"center"}
+      >
         {item.title}
       </Heading>
 
       {item.icon !== null ? (
-        <Stack direction={"row"} spacing={"5"}>
+        <Stack direction={"row"} spacing={"5"} justifyContent={"center"}>
           {item.icon.map((items, index) => (
-            <Icon
+            <Box
+              as={Link}
               key={index}
-              as={items.icon}
-              boxSize={"10"}
-              color={items.color}
-            ></Icon>
+              bgGradient={"linear(to-t, gray.300, gray.200)"}
+              rounded={"2xl"}
+              padding={"5"}
+              isExternal
+              href={items.link}
+            >
+              <Icon as={items.icon} boxSize={"10"} color={items.color}></Icon>
+            </Box>
           ))}
         </Stack>
       ) : null}
@@ -33,9 +52,19 @@ const ItemCard = ({ item }) => {
 const TechnologiesSection = () => {
   return (
     <>
-      <Box my={"20"} as={"section"} id={"technologies"}>
-        <Heading mb={"10"}>Technologies</Heading>
-        <Flex flexDirection={{ base: "column", md: "row" }} gap={"10"} p={"5"}>
+      <Box my={"5"} pt={"10"} as={"section"} id={"technologies"}>
+        <Heading mb={"5"} fontSize={{ base: "4xl", md: "5xl" }}>
+          Technologies
+        </Heading>
+        <Text>
+          In my Journey as a <Text as={"b"}>Front End Web Developer</Text>, so
+          far this are the technologies that I used.
+        </Text>
+        <Flex
+          flexDirection={{ base: "column", md: "row" }}
+          gap={"10"}
+          mt={"16"}
+        >
           {items.map((item, index) => (
             <ItemCard key={index} item={item} />
           ))}
