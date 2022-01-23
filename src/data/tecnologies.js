e926e19df4deca3445c41220f283a029ba7c19e5
Junel <sacrojunel@gmail.com>
2022-01-23 20:09:03 +0800

    chore: add link for icons

--- src/data/tecnologies.js
+++ src/data/tecnologies.js
@@ -11,23 +11,31 @@ export const items = [
   {
     title: "Front End",
     icon: [
-      { icon: FaReact, color: "#1788AD" },
-      { icon: SiNextdotjs, color: "gray.100" },
+      { icon: FaReact, color: "#1788AD", link: "https://reactjs.org/" },
+      { icon: SiNextdotjs, color: "gray.900", link: "https://nextjs.org/" },
     ],
   },
   {
     title: "Library",
     icon: [
-      { icon: SiChakraui, color: "#65CACC" },
-      { icon: SiStyledcomponents, color: "#9B653D" },
+      { icon: SiChakraui, color: "#65CACC", link: "https://chakra-ui.com/" },
+      {
+        icon: SiStyledcomponents,
+        color: "#9B653D",
+        link: "https://styled-components.com/",
+      },
     ],
   },
   {
     title: "Tools",
     icon: [
-      { icon: SiGit, color: "#E64A19" },
-      { icon: SiVisualstudiocode, color: "#22A5F1" },
-      { icon: SiGithub, color: "gray.100" },
+      { icon: SiGit, color: "#E64A19", link: "https://git-scm.com/" },
+      {
+        icon: SiVisualstudiocode,
+        color: "#22A5F1",
+        link: "https://code.visualstudio.com/",
+      },
+      { icon: SiGithub, color: "gray.900", link: "https://github.com/" },
     ],
   },
 ];
