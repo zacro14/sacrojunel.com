3e5a21b8ca424c0f8bb644ec2c224a8980caea05
Junel <sacrojunel@gmail.com>
2022-01-25 00:03:10 +0800

    feat: add gradient to project section

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -1,19 +1,24 @@
 import { Box, Heading, Stack } from "@chakra-ui/react";
 import Project from "./ProjectCard";
 import { project } from "../data/projects";
+import { AboveTheFoldGradient, SpanGradient } from "./gradient";
+import { AnimatePresence } from "framer-motion";
 
 const ProjectsSection = () => {
   return (
     <>
+      <SpanGradient />
       <Box as={"section"} id={"projects"} height={"auto"} pt={"10"}>
         <Heading fontSize={{ base: "4xl", md: "5xl" }}>
           Featured Projects
         </Heading>
         <Box mt={"16"}>
           <Stack>
-            {project.map((project) => (
-              <Project key={project.id} data={project} />
-            ))}
+            <AnimatePresence>
+              {project.map((project) => (
+                <Project key={project.id} data={project} />
+              ))}
+            </AnimatePresence>
           </Stack>
         </Box>
       </Box>
